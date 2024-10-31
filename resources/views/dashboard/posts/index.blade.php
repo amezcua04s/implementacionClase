03817338a2648c57b465cccd51bfba3c9386faa2
amezcua <santiago_amezcua@hotmail.com>
2024-10-31 12:28:03 -0600

    Creacion de modal a la hora de eliminar

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -28,11 +28,33 @@
                   Editar
                 </a>
                 {{-- method="POST" <- Incluir ese parametro en form, despues de la ruta--}}
-                  <form action="{{ route('post.destroy', $post->id) }}" method="POST">
-                    @method('DELETE')
-                    @csrf
-                    <button class="btn btn-danger"> Eliminar </button>
-                  </form>
+                <form action="{{ route('post.destroy', $post->id) }}" method="POST">
+                
+                  <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Eliminar</button>
+
+                  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5" id="exampleModalLabel">¿Desea eliminar este post?</h1>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          Si acepta, ya no se podrán reestablecer los cambios...
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">noooOO</button>
+                          @csrf
+                          @method('DELETE')
+                          <button type="submit" class="btn btn-danger">Si quiero eliminarlo</button>
+                        </div>
+                      </div>
+                    </div>
+              
+                  </div>
+                 
+                </form>
+          
               </td>
             </tr>
             @endforeach
